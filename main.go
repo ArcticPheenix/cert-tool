1f97250f2abab1375cad86a6655834ef7859f677
ArcticPheenix <phoenix.ckelly@gmail.com>
2015-10-12 16:57:06 -0600

    Initial checkin of main.go

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"flag"
+)
+
+func main() {
+	pSelfSignedOpt := flag.Bool("selfsigned", false, "Self signed certificate generation.")
+	pKeySize := flag.Int("size", 2048, "Key size in bits.")
+	pSignCert := flag.Bool("sign", false, "Process certificate signing request, and produce a signed cert.")
+	pCertificateSignReq := flag.Bool("csr", false, "Create a certificate signing request.")
+	pSigningCert := flag.String("signing-cert", "", "Path to certificate to use as a signing cert.")
+	pDays := flag.Int("days", 365, "Number of days that cert will be valid.")
+	positionalArgs := flag.Args()
+	flag.Parse()
+	fmt.Println("selfsigned: ", *pSelfSignedOpt)
+	fmt.Println("size: ", *pKeySize)
+	fmt.Println("sign: ", *pSignCert)
+	fmt.Println("csr: ", *pCertificateSignReq)
+	fmt.Println("signing-cert: ", *pSigningCert)
+	fmt.Println("days: ", *pDays)
+	fmt.Println("args: ", positionalArgs)
+}
+
+func generateCSR(filename string) {
+	if (filename == "") {
+		filename = "newcert.csr"
+	}
+	
+}
