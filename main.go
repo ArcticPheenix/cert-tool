793edcc0a8285e6cad91d671296cc7017fa4645d
ArcticPheenix <phoenix.ckelly@gmail.com>
2018-04-19 10:34:06 -0600

    Initial logic for CreateCertBundle call.

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
-	"flag"
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
+
+type CertSignRequest struct {
+	Country            string `json:"country"`
+	State              string `json:"state"`
+	City               string `json:"city"`
+	Organization       string `json:"organization"`
+	OrganizationalUnit string `json:"organizationalUnit"`
+	CommonName         string `json:"commonName"`
+	EmailAddress       string `json:"emailAddress"`
+	Days               string `json:"days"`
+}
 
 func check(e error) {
 	if e != nil {
@@ -11,23 +26,33 @@ func check(e error) {
 	}
 }
 
-func main() {
-	// Setting up command line parameter flags.
-	pSelfSignedOpt := flag.Bool("selfsigned", false, "Self signed certificate generation.")
-	pKeySize := flag.Int("size", 2048, "Key size in bits.")
-	pSignCert := flag.Bool("sign", false, "Process certificate signing request, and produce a signed cert.")
-	pCertificateSignReq := flag.Bool("csr", false, "Create a certificate signing request.")
-	pSigningCert := flag.String("signing-cert", "", "Path to certificate to use as a signing cert.")
-	pDays := flag.Int("days", 365, "Number of days that cert will be valid.")
-	positionalArgs := flag.Args()
-	flag.Parse()
+var certSignRequests []CertSignRequest
 
-	// Testing output
-	fmt.Println("selfsigned: ", *pSelfSignedOpt)
-	fmt.Println("size: ", *pKeySize)
-	fmt.Println("sign: ", *pSignCert)
-	fmt.Println("csr: ", *pCertificateSignReq)
-	fmt.Println("signing-cert: ", *pSigningCert)
-	fmt.Println("days: ", *pDays)
-	fmt.Println("args: ", positionalArgs)
+const NUM_DAYS_DEFAULT = "730"
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/gencert", CreateCertBundle).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func CreateCertBundle(w http.ResponseWriter, r *http.Request) {
+	//TODO: Create an OpenSSL cert bundle with the given data.
+	params := mux.Vars(r)
+	fmt.Println(r)
+	var certSignRequest CertSignRequest
+	_ = json.NewDecoder(r.Body).Decode(&certSignRequest)
+	certSignRequest.Country = params["country"]
+	certSignRequest.State = params["state"]
+	certSignRequest.City = params["city"]
+	certSignRequest.Organization = params["organization"]
+	certSignRequest.OrganizationalUnit = params["organizationalUnit"]
+	certSignRequest.CommonName = params["commonName"]
+	certSignRequest.EmailAddress = params["emailAddress"]
+	if params["days"] != "" {
+		certSignRequest.Days = params["days"]
+	} else {
+		certSignRequest.Days = "730"
+	}
+	json.NewEncoder(w).Encode(certSignRequest)
 }
